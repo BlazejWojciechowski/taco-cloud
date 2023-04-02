6fc57ce37d5ed36fbf0b094f57f7249ca60bb908
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-04-02 19:47:32 +0200

    initial commit

---
+++ src/test/java/blaze/springframework/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package blaze.springframework.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
