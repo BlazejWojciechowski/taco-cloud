cb328eeb4b0b3977680afb97882e76cbc5b93496
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-04-02 20:00:39 +0200

    added simple POJO's

---
+++ src/main/java/blaze/springframework/tacocloud/domain/Taco.java
@@ -0,0 +1,11 @@
+package blaze.springframework.tacocloud.domain;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+    private String name;
+    private List<String> ingredients;
+}
